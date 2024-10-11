b144e71a61bd2fecb658fbd98ea7a57a53ea17e4
mesairs <115340772+mesairs@users.noreply.github.com>
2024-10-11 08:26:49 +0800

    Add files via upload

--- javas.js
+++ javas.js
@@ -15,7 +15,11 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 1,
       spaceBetween: 5
     }
-  }
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 var swiper = new Swiper(".EventSlider", {
